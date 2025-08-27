28a7c4516dd413934b5457d8a2cff987bef30930
Brian Letting <brianletting46@gmail.com>
2025-08-27 21:31:17 +0300

    Addind tailwind css and making sure if working

--- book-library/vite.config.js
+++ book-library/vite.config.js
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
 })
+// This file is used to configure Vite, a build tool for modern web projects.
